08c5b3905202dc48d408276077b80e02664dfb8d
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-01-23 16:43:19 -0300

    Adjust in styles

--- src/components/template/Main.jsx
+++ src/components/template/Main.jsx
@@ -6,7 +6,7 @@ export default (props) => (
   <>
     <Header {...props} />
     <main className="content container-fluid">
-      <div className="p-3 mt-3">{props.children}</div>
+      <div className="container-sm p-3 mt-3">{props.children}</div>
     </main>
   </>
 );
