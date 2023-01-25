981fb304cce059be2d7ffd827b131d0fd4ebc2bb
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-01-25 15:28:28 -0300

    Refactoring code and create components Form, RowsTable and table

--- src/components/car/CarCrud.jsx
+++ src/components/car/CarCrud.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Main from "../template/Main";
+import Table from "./components/Table";
+import Form from "./components/Form";
+import RowsTable from "./components/RowsTable";
 
 const headerProps = {
   icon: "car",
@@ -99,178 +102,15 @@ export default function CarCrud() {
     });
   };
 
-  const renderFrom = () => {
-    return (
-      <div className="form">
-        <div className="row">
-          <h1>Dados do carro</h1>
-          <div className="col-12 col-md-6">
-            <div className="form-group">
-              <label>Nome</label>
-              <input
-                type="text"
-                className="form-control"
-                name="name"
-                value={values.name}
-                onChange={handleChangeValue}
-                // placeholder="Digite o nome..."
-              />
-            </div>
-          </div>
-
-          <div className="col-12 col-md-6">
-            <div className="form-group">
-              <label>Marca</label>
-              <input
-                type="text"
-                className="form-control"
-                name="marca"
-                value={values.marca}
-                onChange={handleChangeValue}
-                // placeholder="Digite a marca..."
-              />
-            </div>
-          </div>
-
-          <div className="col-12 col-md-6">
-            <div className="form-group">
-              <label>Ano</label>
-              <input
-                type="date"
-                className="form-control"
-                name="year_manufacture"
-                value={values.year_manufacture}
-                onChange={handleChangeValue}
-              />
-            </div>
-          </div>
-
-          <div className="col-12 col-md-6 mb-4">
-            <div className="form-group">
-              <label>Descrição</label>
-              <input
-                type="text"
-                className="form-control"
-                name="description"
-                value={values.description}
-                onChange={handleChangeValue}
-                // placeholder="Descrição..."
-              />
-            </div>
-          </div>
-
-          <hr />
-
-          <h1>Dados do proprietário</h1>
-
-          <div className="col-12 col-md-6 mb-3">
-            <div className="form-group">
-              <label>Nome</label>
-              <input
-                type="text"
-                className="form-control"
-                name="name_user"
-                value={values.name_user}
-                onChange={handleChangeValue}
-                // placeholder="Descrição..."
-              />
-            </div>
-          </div>
-
-          <div className="col-12 col-md-6 mb-3">
-            <div className="form-group">
-              <label>Email</label>
-              <input
-                type="email"
-                className="form-control"
-                name="email_user"
-                value={values.email_user}
-                onChange={handleChangeValue}
-                // placeholder="Descrição..."
-              />
-            </div>
-          </div>
-
-          <div className="col-12 col-md-6 mb-4">
-            <div className="form-group">
-              <label>Telefone</label>
-              <input
-                type="text"
-                className="form-control"
-                name="phone_user"
-                value={values.phone_user}
-                onChange={handleChangeValue}
-                // placeholder="Descrição..."
-              />
-            </div>
-          </div>
-
-          <hr />
-
-          <div className="row">
-            <div className="col-12 d-flex justify-content-end">
-              <button className="btn btn-primary" onClick={handleSave}>
-                Salvar
-              </button>
-
-              <button className="btn btn-secondary ms-2" onClick={handleClean}>
-                Cancelar
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  const renderTable = () => {
-    return (
-      <div className="table-responsive">
-        <table className="table mt-4">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Nome</th>
-              <th>Marca</th>
-              <th>Ano</th>
-              <th>Descrição</th>
-              <th>Nome</th>
-              <th>Email</th>
-              <th>Telefone</th>
-              <th>Ações</th>
-            </tr>
-          </thead>
-          <tbody>{renderRows()}</tbody>
-        </table>
-      </div>
-    );
-  };
-
   const renderRows = () => {
     {
       return listCars.data?.map((car) => {
         return (
-          <tr key={car.id}>
-            <td>{car.id}</td>
-            <td>{car.name}</td>
-            <td>{car.marca}</td>
-            <td>{car.year_manufacture}</td>
-            <td>{car.description}</td>
-            <td>{car.name_user}</td>
-            <td>{car.email_user}</td>
-            <td>{car.phone_user}</td>
-            <td>
-              <button className="btn btn-warning" onClick={() => load(car)}>
-                <i className="fa fa-pencil"></i>
-              </button>
-              <button className="btn btn-danger ms-2">
-                <i
-                  className="fa fa-trash"
-                  onClick={() => handleRemove(car)}
-                ></i>
-              </button>
-            </td>
-          </tr>
+          <RowsTable
+            car={car}
+            onClickLoad={load}
+            onClickHandleRemove={handleRemove}
+          />
         );
       });
     }
@@ -278,8 +118,13 @@ export default function CarCrud() {
 
   return (
     <Main {...headerProps}>
-      {renderFrom()}
-      {renderTable()}
+      <Form
+        values={values}
+        handleChangeValue={handleChangeValue}
+        onClickClean={handleClean}
+        onClickSave={handleSave}
+      />
+      <Table renderRows={renderRows} />
     </Main>
   );
 }
