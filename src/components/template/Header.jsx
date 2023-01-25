f98c6e50e212f7f8d5d25e236c3f80f212f58205
LeozinhoPortoM <leo.porto.lpm@hotmail.com>
2023-01-25 15:31:36 -0300

    Including logo in header

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -1,11 +1,17 @@
-import './Header.css'
+import "./Header.css";
 
-import React from 'react'
+import React from "react";
+import Logo from "./Logo";
 
-export default props =>
-<header className="header d-sm-flex flex-column">
-    <h1 className='mt-3'>
-        <i className={`fa fa-${props.icon}`}></i>{props.title}
-    </h1>
-    <p className='lead text-muted'>{props.subtitle}</p>
-</header>
+export default (props) => (
+  <header className="header d-sm-flex flex">
+    <Logo />
+    <div>
+      <h1 className="mt-3">
+        <i className={`fa fa-${props.icon}`}></i>
+        {props.title}
+      </h1>
+      <p className="lead text-muted">{props.subtitle}</p>
+    </div>
+  </header>
+);
